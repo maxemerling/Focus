797311e8ef2da166ce5cb8d097ace7193e33f3c4
Katniss Lee <jihyunlee@berkeley.edu>
2019-10-16 22:12:57 -0700

    progress in adding to list2

--- app/src/main/java/com/example/focus/ListActivity.java
+++ app/src/main/java/com/example/focus/ListActivity.java
@@ -1,14 +1,39 @@
 package com.example.focus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
+
+    ArrayList<String> locations = new ArrayList<>();
+    RecyclerView recyclerView;
+    MyAdapter adapter;
+    Context context = getApplicationContext();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location_string");
+        locations.add(location);
+
+        recyclerView = (RecyclerView) findViewById(R.id.focus_location_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new MyAdapter(locations, context);
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/focus/MyAdapter.java
@@ -0,0 +1,7 @@
+package com.example.focus;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapater<MyHolder>{
+
+}
